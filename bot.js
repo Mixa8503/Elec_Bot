f27602b49d96724f52cc590debe2bf675d06523d
Mixa8503 <miki21405242@gmail.com>
2023-01-14 20:06:22 +0200

    added text.js modified bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const cheerio = require('cheerio');
 const request = require('request');
-const temp = require('./temp')
+const text = require('./text')
 const { token } = require("./token");
 const { default: axios } = require('axios');
+const { helpCommand } = require('./text');
 
 const Token = token.tok;
 // const url = "http://g20.k.vu/";
@@ -13,40 +14,44 @@ const bot = new TelegramBot(Token, { polling: true });
 
 console.log('Bot has been started..')
 
-const parse = async () => {
-    const getHTML = async(url) => {
-        const { data } = await axios.get(url);
-        return cheerio.load(data);
-    };
-    const $ = await getHTML('http://g20.k.vu/');
-    const LightStatus = $('#main > b > font').text().trim();
-    const DurationOnlineOffline = $("body > p:nth-child(8) > span").text();
-
-    if(LightStatus === 'YES'){
-        bot.onText(/\/check/, (msg) => {
-            // const { id } = msg.chat;
-            bot.sendMessage(msg.chat.id, "Світло є!");
-        });
-    }
-    else{
-        bot.onText(/\/check/, (msg) => {
-            bot.sendMessage(msg.chat.id, "Світла нема!");
-        });
-    }
-
-    bot.onText(/\/duration/, (msg) => {
-        bot.sendMessage(msg.chat.id, DurationOnlineOffline);
-    });
-};
-
-parse();
-
 bot.onText(/\/start/, (msg) => {
 
-    bot.sendMessage(msg.chat.id, "Hello, " + msg.from.first_name);
-});
+    bot.sendMessage(msg.chat.id, "Привіт, " + msg.from.first_name + ". " + helpCommand);
 
-bot.onText(/\/ping/, msg => {
-    const { id } = msg.chat;
-    bot.sendMessage(id, "pong");
+const parse = async () => {
+        const getHTML = async(url) => {
+            const { data } = await axios.get(url);
+            return cheerio.load(data);
+        };
+        const $ = await getHTML('http://g20.k.vu/');
+        const LightStatus = $('#main > b > font').text().trim();
+        const DurationOnlineOffline = $("body > p:nth-child(8) > span").text();
+
+        if(LightStatus === 'YES'){
+            bot.onText(/\/check/, (msg) => {
+                bot.sendMessage(msg.chat.id, text.positiveRecall);
+            });
+            bot.onText(/\/duration/, (msg) => {
+                bot.sendMessage(msg.chat.id,"Світло є вже: " + DurationOnlineOffline);
+            });
+        }
+        else{
+            bot.onText(/\/check/, (msg) => {
+                bot.sendMessage(msg.chat.id, text.negativeRecall);
+            });
+            bot.onText(/\/duration/, (msg) => {
+                bot.sendMessage(msg.chat.id,"Світлa немає вже: " + DurationOnlineOffline);
+            });
+        }
+    };
+
+    bot.onText(/\/help/, msg => {
+        bot.sendMessage(msg.chat.id, "None here yet");
+    });
+
+    bot.onText(/\/ping/, msg => {
+        bot.sendMessage(msg.chat.id, "pong");
+    });
+    parse();
+    //"body > details:nth-child(13) > pre"
 });
---
+++ text.js
@@ -0,0 +1,7 @@
+const help = '/help';
+
+module.exports = {
+    positiveRecall: "Світло є!",
+    negativeRecall: "Світла нема!",
+    helpCommand: `Напиши ${help} щоб дізнатись усі доступні команди.`
+};
