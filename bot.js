d9586a413e20e4fcbc96692779005b0758e26cff
Mixa8503 <miki21405242@gmail.com>
2023-01-14 16:45:08 +0200

    temp file commented out, added temp.js to bot.js

--- bot.js
+++ bot.js
@@ -3,15 +3,50 @@
 const TelegramBot = require('node-telegram-bot-api');
 const cheerio = require('cheerio');
 const request = require('request');
-const {token} = require("./token");
+const temp = require('./temp')
+const { token } = require("./token");
 const { default: axios } = require('axios');
+
 const Token = token.tok;
+// const url = "http://g20.k.vu/";
+const bot = new TelegramBot(Token, { polling: true });
 
 console.log('Bot has been started..')
 
-const bot = new TelegramBot(Token, { polling: true });
+const parse = async () => {
+    const getHTML = async(url) => {
+        const { data } = await axios.get(url);
+        return cheerio.load(data);
+    };
+    const $ = await getHTML('http://g20.k.vu/');
+    const LightStatus = $('#main > b > font').text().trim();
+    const DurationOnlineOffline = $("body > p:nth-child(8) > span").text();
 
-bot.on('message', (msg) => {
+    if(LightStatus === 'YES'){
+        bot.onText(/\/check/, (msg) => {
+            // const { id } = msg.chat;
+            bot.sendMessage(msg.chat.id, "Світло є!");
+        });
+    }
+    else{
+        bot.onText(/\/check/, (msg) => {
+            bot.sendMessage(msg.chat.id, "Світла нема!");
+        });
+    }
+
+    bot.onText(/\/duration/, (msg) => {
+        bot.sendMessage(msg.chat.id, DurationOnlineOffline);
+    });
+};
+
+parse();
+
+bot.onText(/\/start/, (msg) => {
 
     bot.sendMessage(msg.chat.id, "Hello, " + msg.from.first_name);
+});
+
+bot.onText(/\/ping/, msg => {
+    const { id } = msg.chat;
+    bot.sendMessage(id, "pong");
 });
--- temp.js
+++ temp.js
@@ -1,14 +1,31 @@
-const cheerio = require('cheerio');
-const request = require('request');
-const axios = require('axios');
+// const cheerio = require('cheerio');
+// const request = require('request');
+// const axios = require('axios');
 
-const parse = async () => {
-    const getHTML = async(url) => {
-        const { data } = await axios.get(url);
-        return cheerio.load(data);
-    };
-    const $ = await getHTML('http://g20.k.vu/');
-    console.log($.html());
-};
+// request('http://g20.k.vu/', function (error, response, html) {
+// if (!error && response.statusCode == 200) {
+// console.log(html);
+// }
+// });
 
-parse();
+// const parse = async () => {
+//     const getHTML = async(url) => {
+//         const { data } = await axios.get(url);
+//         return cheerio.load(data);
+//     };
+//     const $ = await getHTML('http://g20.k.vu/');
+//     //1 reply markup
+//     const LightStatus = $('#main > b > font').text().trim();
+//     if(LightStatus === 'YES'){
+//         console.log("Світло є!");
+//     }
+//     else{
+//         console.log("Світла нема :(");
+//     }
+
+//     //2 reply markup
+//     const DurationOnlineOffline = $("body > p:nth-child(8) > span").text().trim();
+//     console.log(DurationOnlineOffline);
+// };
+
+// parse();
