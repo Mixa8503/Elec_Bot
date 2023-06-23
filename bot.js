294af135c5c225f5794ce749398fd5ea8740cf23
Mixa8503 <miki21405242@gmail.com>
2023-06-24 00:40:57 +0300

    feat: add water news

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
-'use srtict';
+'use strict';
 
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+const { debug, getData } = require('./func');
 const cheerio = require('cheerio');
-const text = require('./text')
-const { debug, getData} = require('./func')
-const { token } = require("./token");
-const { default: axios } = require('axios');
+const https = require('https');
+const { token } = require('./token');
 const { helpCommand, helpCommands, invalidInput } = require('./text');
 
 const Token = token.tok;
@@ -13,71 +13,101 @@ const bot = new TelegramBot(Token, { polling: true });
 
 console.log('Bot has been started..');
 
-let checker = true;
-
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Привіт, " + msg.from.first_name + ". " + helpCommand);
-    if(checker){
-        parse();
-        checker = false;
-    }
+  bot.sendMessage(msg.chat.id, `Привіт, ${msg.from.first_name}. ${helpCommand}`);
 });
 
-    async function parse() { 
-        const getHTML = async(url) => {
-            const { data } = await axios.get(url);
-            return cheerio.load(data);
-        };
-        const $ = await getHTML('http://g20.k.vu/');
-        const LightStatus = $('#main > b > font').text().trim();
-        const DurationOnlineOffline = $('span.v').text();
-        const historyOnOff = $('body > details > pre').text();
-        const updatedhistory = historyOnOff.split('+0200');
-        const newArr = updatedhistory.map(el => el.trim()).filter(el => el !== '');
+const axiosInstance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
+});
 
-        const dates = {};
-        for(const el of newArr) {
-            const [ date ] = el.match(/\b\w{3}, \d{1,2} \w{3} \d{4}\b/g);
-            const [ status, time ] = el.split(date).map(el => el.trim());
-            if(dates[date] === undefined || null) {
-                dates[date] = {};
-            }
-            dates[date][time] = status;
+const websites = [
+  {
+    name: 'Electricity',
+    url: 'http://g20.k.vu/',
+    parse: async () => {
+      const { data } = await axiosInstance.get(websites[0].url);
+      const $ = cheerio.load(data);
+      const lightStatus = $('#main > b > font').text().trim();
+      const durationOnlineOffline = $('span.v').text();
+      const historyOnOff = $('body > details > pre').text();
+      const updatedhistory = historyOnOff.split('+0200');
+      const newArr = updatedhistory.map(el => el.trim()).filter(el => el !== '');
+
+      const dates = {};
+      for (const el of newArr) {
+        const [date] = el.match(/\b\w{3}, \d{1,2} \w{3} \d{4}\b/g);
+        const [status, time] = el.split(date).map(el => el.trim());
+        if (dates[date] === undefined || null) {
+          dates[date] = {};
         }
+        dates[date][time] = status;
+      }
 
-        if(LightStatus === 'YES'){
-            bot.onText(/\/check/, (msg) => {
-                bot.sendMessage(msg.chat.id, text.positiveRecall);
-            });
-            bot.onText(/\/duration/, (msg) => {
-                bot.sendMessage(msg.chat.id,"Світло є вже: " + DurationOnlineOffline);
-            });
-        }
-        else{
-            bot.onText(/\/check/, (msg) => {
-                bot.sendMessage(msg.chat.id, text.negativeRecall);
-            });
-            bot.onText(/\/duration/, (msg) => {
-                bot.sendMessage(msg.chat.id,"Світлa немає вже: " + DurationOnlineOffline);
-            });
-        }
-        bot.onText(/\/history (.+)/, (msg, [source, match]) => {
-            if(dates[match] != undefined){
-                const out = `${match}\n${getData(match, dates)}`;
-                bot.sendMessage(msg.chat.id, out);
-            }
-            else{
-                bot.sendMessage(msg.chat.id, invalidInput);
-            }
-        });
+      return {
+        lightStatus,
+        durationOnlineOffline,
+        dates
+      };
+    }
+  },
+  {
+    name: 'Water',
+    url: 'https://vodokanal.kiev.ua/avarijna-sluzhba-1581/',
+    parse: async () => {
+      const { data } = await axiosInstance.get(websites[1].url);
+      const $ = cheerio.load(data);
+      const WaterNews = $("body > div > main > div > div > div.col-lg-9.col-md-9.col-sm-9.col-xs-12 > div > div > div.p-description > div:nth-child(6)").text();
+      return {
+        WaterNews
+      };
+    }
+  }
+];
 
-        bot.onText(/\/history/, (msg) => {
-            if(msg.text == '/history'){
-                bot.sendMessage(msg.chat.id, invalidInput);
-            }
-        });
-    };
-
-    bot.onText(/\/help/, msg => {
-        bot.sendMessage(msg.chat.id, helpCommands);
+websites.forEach((website) => {
+    bot.onText(/\/check/, async (msg) => {
+    const { lightStatus, durationOnlineOffline } = await website.parse();
+    let message;
+    if (lightStatus === 'YES') {
+    message = `Світло є вже: ${durationOnlineOffline}`;
+    } else {
+    message = `Світлa немає вже: ${durationOnlineOffline}`;
+    }
+      bot.sendMessage(msg.chat.id, message);
     });
+  
+    bot.onText(/\/duration/, async (msg) => {
+      const { durationOnlineOffline } = await website.parse();
+      bot.sendMessage(msg.chat.id, `Duration: ${durationOnlineOffline}`);
+    });
+  });
+
+  bot.onText(/\/history (.+)/, async (msg, match) => {
+    const { dates } = await website.parse();
+    if (dates[match[1]] !== undefined) {
+      const out = `${match[1]}\n${getData(match[1], dates)}`;
+      bot.sendMessage(msg.chat.id, out);
+    } else {
+      bot.sendMessage(msg.chat.id, invalidInput);
+    }
+  });
+
+bot.onText(/\/water/, async (msg) => {
+  const { WaterNews } = await websites[1].parse();
+  bot.sendMessage(msg.chat.id, WaterNews);
+});
+
+bot.onText(/\/history/, (msg) => {
+  bot.sendMessage(msg.chat.id, invalidInput);
+});
+
+bot.onText(/\/help/, (msg) => {
+  bot.sendMessage(msg.chat.id, helpCommands);
+});
+
+
+
+
